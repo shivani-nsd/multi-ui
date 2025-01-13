f65c2ca4085fe3173cee85712918c5cb0def7d8b
Shivani Kulandaivel <1095308@apps.nsd.org>
2025-01-13 23:13:35 +0000

    hi

--- src/main/java/edu/bothell/multi_ui/core/Game.java
+++ src/main/java/edu/bothell/multi_ui/core/Game.java
@@ -33,7 +33,6 @@ public class Game {
         char[] pcs = new char[p.size()];
         for(int i = 0; i < pcs.length; i++) 
             pcs[i] = p.get(i).getChar();
-        
         return pcs;
     }
     
@@ -42,6 +41,34 @@ public class Game {
         return s.isOpen(pos) && active.getSId().equals(sId);
     }
 
+  /*   public boolean checkAll(char c){
+        boolean f = false;
+        boolean of = false;
+        boolean df = false;
+        boolean odf = false;
+        for (int x=0; x<3; x++){
+          f = true;
+          of = true;
+          df = true;
+          odf = true;
+          for(int y=0; y<3; y++){
+            if(this.STATE[y][x].returnSymbol()!=c)f = false;
+            if(this.STATE[x][y].returnSymbol()!=c)of = false;
+            if(this.STATE[y][y].returnSymbol()!=c)df = false;
+            if(this.STATE[y][2-y].returnSymbol()!=c)odf = false;
+          }
+          if(f||of||df||odf) return true;
+        }
+          return false;
+        }
+
+    public boolean checkWin(){
+        boolean flag = false;
+        if(this.checkAll('x'))return true;
+        if(this.checkAll('o'))return true;
+        return flag;
+    }
+*/
     public char play(int[] pos, String sId){
         if(!isValid(pos, sId)) return ' ';
         turn++;
